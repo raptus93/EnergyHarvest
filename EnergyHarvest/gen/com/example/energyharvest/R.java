09ae361f228397771c88787bbb5ecb19ba50d8fa
iQew <kjellbunjes@gmail.com>
2014-04-13 19:51:43 +0200

    R.Id.loginEmail und R.Id.loginPassword Werte getauscht

--- EnergyHarvest/gen/com/example/energyharvest/R.java
+++ EnergyHarvest/gen/com/example/energyharvest/R.java
@@ -79,14 +79,13 @@ public final class R {
         public static final int tutorial=0x7f070006;
     }
     public static final class string {
-        public static final int Registrieren=0x7f05001e;
         public static final int action_settings=0x7f050001;
         public static final int answerA=0x7f050002;
         public static final int answerB=0x7f050003;
         public static final int answerC=0x7f050004;
         public static final int answerD=0x7f050005;
         public static final int app_name=0x7f050000;
-        public static final int clan=0x7f050023;
+        public static final int clan=0x7f050022;
         public static final int confirm=0x7f050011;
         public static final int email=0x7f050007;
         public static final int hello_clan=0x7f05000a;
@@ -95,18 +94,18 @@ public final class R {
         public static final int hello_profile=0x7f05000d;
         public static final int hello_registry=0x7f05000f;
         public static final int hello_tutorial=0x7f05000c;
-        public static final int login=0x7f050020;
-        public static final int logodescription=0x7f050021;
+        public static final int login=0x7f05001f;
+        public static final int logodescription=0x7f050020;
         public static final int main_menu=0x7f050010;
-        public static final int map=0x7f050022;
+        public static final int map=0x7f050021;
         public static final int password=0x7f050008;
         public static final int password_incorrect=0x7f050013;
-        public static final int placeholder_3d_map_tracking=0x7f050028;
-        public static final int placeholder_image_tracking=0x7f050027;
-        public static final int placeholder_quiz=0x7f050026;
-        public static final int profile=0x7f050024;
+        public static final int placeholder_3d_map_tracking=0x7f050027;
+        public static final int placeholder_image_tracking=0x7f050026;
+        public static final int placeholder_quiz=0x7f050025;
+        public static final int profile=0x7f050023;
         public static final int question=0x7f050006;
-        public static final int register=0x7f05001f;
+        public static final int register=0x7f05001e;
         public static final int title_activity_clan=0x7f050015;
         public static final int title_activity_demo=0x7f05001d;
         public static final int title_activity_fight=0x7f05001c;
@@ -117,8 +116,8 @@ public final class R {
         public static final int title_activity_quiz=0x7f05001b;
         public static final int title_activity_registry=0x7f05001a;
         public static final int title_activity_tutorial=0x7f050019;
-        public static final int title_registry=0x7f050029;
-        public static final int tutorial=0x7f050025;
+        public static final int title_registry=0x7f050028;
+        public static final int tutorial=0x7f050024;
         public static final int username=0x7f050009;
         public static final int username_not_found=0x7f050012;
     }
