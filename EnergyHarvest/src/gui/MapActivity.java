b5ca44812865fc834adac2753c43db8b1a408338
iQew <kjellbunjes@gmail.com>
2014-05-12 08:57:46 +0200

    • Grafikupdate

--- EnergyHarvest/src/gui/MapActivity.java
+++ EnergyHarvest/src/gui/MapActivity.java
@@ -1,62 +1,73 @@
 package gui;
 
 import android.app.Activity;
-import android.content.Intent;
-import android.content.pm.ActivityInfo;
-import android.content.res.Configuration;
 import android.os.Bundle;
-import android.support.v4.app.NavUtils;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
 
 import com.example.energyharvest.R;
 
 /**
- * @version 1.1.3 (13/04/2014)
- * @author Kjell Bunjes
+ * @version 1.1.4 (13/04/2014)
+ * @author Katja Lange
  *
  */
 
 public class MapActivity extends Activity {
-
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_map);
-		
-		ActivityHolder.getInstance().setActiveActivity(this);
-	}
-
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.map, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		switch (item.getItemId()) {
-		case android.R.id.home:
-			// This ID represents the Home or Up button. In the case of this
-			// activity, the Up button is shown. Use NavUtils to allow users
-			// to navigate up one level in the application structure. For
-			// more details, see the Navigation pattern on Android Design:
-			//
-			// http://developer.android.com/design/patterns/navigation.html#up-vs-back
-			//
-			NavUtils.navigateUpFromSameTask(this);
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
 	
-	@Override
-	public void onConfigurationChanged(Configuration newConfig) {
-	    super.onConfigurationChanged(newConfig);
-	    setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
-	}
+	private int activeBuilding;
+	private int activeButton;
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+        activeBuilding = 1;
+        activeButton = 1;
+    }
+
+    public void updateMap(View view){
+    	
+    	View mapLayout = findViewById(R.id.map_content); // getting the layout of the map
+    	ImageButton tab_1 = (ImageButton)findViewById(R.id.tab_1); // getting the first tab of the map
+    	ImageButton tab_2 = (ImageButton)findViewById(R.id.tab_2); // getting the second tab of the map
+    	ImageButton tab_3 = (ImageButton)findViewById(R.id.tab_3); // getting the third tab of the map
+    	ImageButton tab_4 = (ImageButton)findViewById(R.id.tab_4); // getting the fourth tab of the map
+    	
+    	if (view.getId() == R.id.button_l1){
+    		mapLayout.setBackgroundResource(R.drawable.map_background_l1); // setting the background
+    		tab_1.setImageResource(R.drawable.ic_element_1); // setting the icon on tab_1
+    		tab_2.setImageResource(R.drawable.ico1);	// setting the icon on tab_2
+    		tab_3.setImageResource(R.drawable.ico2); // setting the icon on tab_3
+    		tab_4.setImageResource(R.drawable.ic_cat_l1_1); // setting the icon on tab_4
+    	} else if(view.getId() == R.id.button_l2){
+    		mapLayout.setBackgroundResource(R.drawable.map_background_l2);
+    		tab_1.setImageResource(R.drawable.ic_element_2);
+    		tab_2.setImageResource(R.drawable.ic_cat_l2_1);
+    		tab_3.setImageResource(R.drawable.ic_cat_l2_1);
+    		tab_4.setImageResource(R.drawable.ic_cat_l2_1);
+    	} else if(view.getId() == R.id.button_l3){
+    		mapLayout.setBackgroundResource(R.drawable.map_background_l3);
+    		tab_1.setImageResource(R.drawable.ic_element_3);
+    		tab_2.setImageResource(R.drawable.ic_cat_l3_1);
+    		tab_3.setImageResource(R.drawable.ic_cat_l3_1);
+    		tab_4.setImageResource(R.drawable.ic_cat_l3_1);
+    	} else if(view.getId() == R.id.button_l4){
+    		mapLayout.setBackgroundResource(R.drawable.map_background_l4);
+    		tab_1.setImageResource(R.drawable.ic_element_4);
+    		tab_2.setImageResource(R.drawable.ic_cat_l4_1);
+    		tab_3.setImageResource(R.drawable.ic_cat_l4_1);
+    		tab_4.setImageResource(R.drawable.ic_cat_l4_1);
+    	}
+    }
+   
+    
+    
+    public void UpdateTabContent(){
+	   
+    }
+   
+   
 }
